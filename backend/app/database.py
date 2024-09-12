4542797d8c3f7cc08d123fb32c5a903ecdca166e
kevin-pek <76090938+kevin-pek@users.noreply.github.com>
2024-09-13 01:47:00 +0800

    Update DATABASE_URL environment variable config for both docker and local run

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,10 +3,11 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv('DATABASE_URL', "postgresql://postgres:password@db/postgres")
+DATABASE_URL = os.getenv('DATABASE_URL', "postgresql://postgres:password@localhost/postgres")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 metadata = MetaData()
+
