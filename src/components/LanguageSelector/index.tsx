3a1fe950b85a42bccd37c5d40a775967381b32f6
clevon-w <e0726468@u.nus.edu>
2024-09-23 02:50:29 +0800

    cleaning up the switch language selector because it was really ugly previously

--- src/components/LanguageSelector/index.tsx
+++ src/components/LanguageSelector/index.tsx
@@ -1,27 +1,46 @@
-import { Select } from 'antd';
+import { CaretDownOutlined, GlobalOutlined } from '@ant-design/icons';
+import { getLocale, setLocale } from '@umijs/max'; // Adjusted import
+import { Dropdown, Menu } from 'antd';
 import React from 'react';
-import { getLocale, setLocale } from 'umi';
-
-const { Option } = Select;
 
 const LanguageSelector: React.FC = () => {
   const currentLocale = getLocale();
 
-  const handleChange = (value: string) => {
-    setLocale(value, false); // 'false' to avoid full page reload
+  const handleMenuClick = (e: { key: string }) => {
+    setLocale(e.key, false);
   };
 
+  const languageMap = {
+    'en-US': 'English',
+    'zh-CN': '中文',
+    'ms-MY': 'Melayu',
+    'ta-IN': 'தமிழ்',
+  };
+
+  const languageMenu = (
+    <Menu onClick={handleMenuClick} selectedKeys={[currentLocale]}>
+      <Menu.Item key="en-US">{languageMap['en-US']}</Menu.Item>
+      <Menu.Item key="zh-CN">{languageMap['zh-CN']}</Menu.Item>
+      <Menu.Item key="ms-MY">{languageMap['ms-MY']}</Menu.Item>
+      <Menu.Item key="ta-IN">{languageMap['ta-IN']}</Menu.Item>
+    </Menu>
+  );
+
   return (
-    <Select
-      value={currentLocale}
-      onChange={handleChange}
-      style={{ width: 120 }}
-    >
-      <Option value="en-US">English</Option>
-      <Option value="zh-CN">中文</Option>
-      <Option value="ms-MY">Bahasa Melayu</Option>
-      <Option value="ta-IN">தமிழ்</Option>
-    </Select>
+    <Dropdown overlay={languageMenu} placement="bottomRight">
+      <span
+        style={{
+          cursor: 'pointer',
+          display: 'inline-flex',
+          alignItems: 'center',
+          padding: '0 8px',
+        }}
+      >
+        <GlobalOutlined />
+        <span style={{ marginLeft: 4 }}>{languageMap[currentLocale]}</span>
+        <CaretDownOutlined />
+      </span>
+    </Dropdown>
   );
 };
 
