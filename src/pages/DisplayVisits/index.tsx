bb4bd32671be48109a1c23f08ada788787a50619
kevin-pek <76090938+kevin-pek@users.noreply.github.com>
2024-09-28 10:52:22 +0800

    time

--- src/pages/DisplayVisits/index.tsx
+++ src/pages/DisplayVisits/index.tsx
@@ -509,7 +509,7 @@ const DisplayVisitsPage: React.FC = () => {
                           (
                           <Text strong style={{ fontSize: 12}}>
                             {visit.submission_time
-                              ? `${dayjs().add(8, 'hour').to(dayjs(visit.submission_time))}`
+                              ? `${dayjs().add(8, 'hour').to(dayjs(visit.submission_time).add(8, 'hour'))}`
                               : 'None'}
                           </Text>
                           )
